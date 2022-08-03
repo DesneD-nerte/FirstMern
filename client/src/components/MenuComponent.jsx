22084ddc9e7203d69453baa6036da22c715cb5f9
Alexandr Ershov <ershov.avdd@gmail.com>
2022-08-04 00:12:35 +0700

    some udaptive for profile page

--- client/src/components/MenuComponent.jsx
+++ client/src/components/MenuComponent.jsx
@@ -31,15 +31,13 @@ export default function MenuComponent() {
     };
 
     return (
-        <div className="wrapperMenu">
-            <div className="elementsMenu">
-                <Button onClick={schedule}>Расписание</Button>
-                <Button onClick={journal}>Журнал посещений</Button>
-                <Button onClick={news}>Лента новостей</Button>
-                <Button onClick={myProfile}>Личный кабинет</Button>
+        <div className="elementsMenu">
+            <Button onClick={schedule}>Расписание</Button>
+            <Button onClick={journal}>Журнал посещений</Button>
+            <Button onClick={news}>Лента новостей</Button>
+            <Button onClick={myProfile}>Личный кабинет</Button>
 
-                <Button onClick={logOut}>Выйти</Button>
-            </div>
+            <Button onClick={logOut}>Выйти</Button>
         </div>
     );
 }
